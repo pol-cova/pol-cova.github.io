8880d24f5839e3bc3850591af37c5dd4360a2956
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-02-13 00:10:49 -0600

    feat: valentine card

---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.getElementById("sorpresa").addEventListener("click", function() {
+    // Redirect to another page when the button is clicked
+    window.location.href = "sorpresa.html";
+});
